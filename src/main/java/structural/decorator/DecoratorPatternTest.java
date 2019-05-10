957b14974658e5c180e1e862cff389998e37b9ba
zhengcodes <1281220666@qq.com>
2019-05-10 14:06:36 +0800

    add decorator desc

--- src/main/java/structural/decorator/DecoratorPatternTest.java
+++ src/main/java/structural/decorator/DecoratorPatternTest.java
@@ -3,7 +3,9 @@ package structural.decorator;
 import structural.decorator.impl.Circle;
 import structural.decorator.impl.Rectangle;
 import structural.decorator.impl.RedShapeDecorator;
-
+/**
+ * 装饰器模式（Decorator Pattern）允许向一个现有的对象添加新的功能，同时又不改变其结构。
+ */
 public class DecoratorPatternTest {
     public static void main(String[] args) {
         Shape circle = new Circle();
